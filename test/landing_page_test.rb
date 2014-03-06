2110acd20edaed430c378b808ae28070b42ca12f
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-03-06 16:21:33 -0700

    Added ruby to index

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -47,7 +47,7 @@ describe "Landing page" do
   it "displays the copyright in the footer" do
     footer = page.find('html body footer')
     footer.text.must_match /©/
-    footer.text.must_include "2014"
+    footer.text.must_include Time.now.year.to_s
   end
 
   it "displays an image of the restaurant" do
